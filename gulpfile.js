31499bc080d81b5e8e0f4e2ed0f2ba85ac517a27
Erik Dao <erikddao@gmail.com>
2017-06-01 22:06:51 +0700

    Complete page header

--- gulpfile.js
+++ gulpfile.js
@@ -3,7 +3,7 @@ var gulp = require('gulp');
 var sass = require('gulp-sass');
 
 gulp.task('sass', function() {
-    return gulp.src('assets/sass/main.scss')
+    return gulp.src('assets/sass/**/*.scss')
                .pipe(sass())
                .pipe(gulp.dest('kaec/static/css/'))
 });
